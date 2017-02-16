3a2468bd05699335b8a6774fad3db3fc9e389c73
Nick Chemsak <nchemsak@gmail.com>
2017-02-16 14:40:41 -0600

    added salesAgents SQL

--- brazil_customers_invoices.sql
+++ brazil_customers_invoices.sql
@@ -1,5 +1,4 @@
 SELECT Customer.FirstName, Customer.LastName, Invoice.InvoiceId,  Invoice.InvoiceDate, Invoice.BillingCountry
-FROM Invoice
-INNER JOIN Customer
+FROM Invoice, Customer
 ON Customer.CustomerId = Invoice.CustomerId
 WHERE Customer.Country = 'Brazil'
